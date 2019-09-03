eff552eb96d134aba32f234102ce5361be0793f7
Mergjunarhola <jonbe209@nemi.tskoli.is>
2019-09-03 15:58:31 +0000

    bla bla

---
+++ App.py
@@ -0,0 +1,20 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return '<h1>Flask er flottast</h1><a href="sida1">Sida1</a>'
+
+
+@app.route('/sida1')
+def sida1():
+        return "Síða 1"
+
+
+
+
+
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
